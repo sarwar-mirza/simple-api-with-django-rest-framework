b2ae13b5736114be4dbf7ae30b9eafb66257ab6a
mithu <nilchirkutpoison@gmail.com>
2024-03-16 23:21:12 +0600

    Create database and register showing table

--- enroll/admin.py
+++ enroll/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BusinessAutomationInterne
 
 # Register your models here.
+@admin.register(BusinessAutomationInterne)
+# create class
+class BusinessAutomationInterneAdmin(admin.ModelAdmin):
+    # Ordering table field name
+    list_display = ['id', 'first_name', 'last_name', 'dep', 'city', 'email']
--- enroll/models.py
+++ enroll/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models(database) here.
+class BusinessAutomationInterne(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    dep = models.CharField(max_length=50)
+    city = models.CharField(max_length=70)
+    email = models.EmailField(max_length=100)
+    

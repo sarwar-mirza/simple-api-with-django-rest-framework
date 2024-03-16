44ae7fc66016c0ed9482045e13d8b513cf668f69
mithu <nilchirkutpoison@gmail.com>
2024-03-17 00:08:34 +0600

    Application logic implementation(views.py)

--- enroll/views.py
+++ enroll/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import BusinessAutomationInterne
 
 # Create your views here.
+class InterneRegistrationTemplateView(TemplateView):
+    template_name = 'enroll/interneregistration.html'

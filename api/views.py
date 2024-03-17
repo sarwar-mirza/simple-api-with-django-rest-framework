edeed4ca70fc226bbca509475b2233094306c326
mithu <nilchirkutpoison@gmail.com>
2024-03-17 11:41:53 +0600

    rest framework views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from enroll.models import BusinessAutomationInterne
+from .serializers import BusinessAutomationInterneSerializers
 
 # Create your views here.
+class BusinessAutomationInterneModelViewSets(viewsets.ModelViewSet):
+    
+    queryset = BusinessAutomationInterne.objects.all()  # Retrieve all instances of MyModel from the database
+    serializer_class = BusinessAutomationInterneSerializers  # Convert [python data to json data]
